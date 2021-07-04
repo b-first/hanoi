d4a6e59f2e39f4da585f40573685348aa46259e6
b-first <brandon.fuerst@gmail.com>
2021-07-04 09:55:09 -0400

    CPU player

---
+++ CPU_Player.py
@@ -0,0 +1,56 @@
+# Return top piece in the stack, 0 if no pieces
+def getTopPiece(stack):
+    try: return min(stack.keys())
+    except: return 0
+
+def checkOdd(num):
+    return num % 2
+
+#def move(stacks, start, end):
+#    stacks[end-1].append(stacks[start-1].pop())
+#    return end-1
+
+# CPU Move
+def CPUMove(stacks, last_move):
+    # Get the number top of stack or 0 if empty stack
+    one = getTopPiece(stacks[0])
+    two = getTopPiece(stacks[1])
+    three = getTopPiece(stacks[2])
+    
+    # First move
+    if two == 0 and three == 0:             # If two and three are empty, then it's the first turn
+        level = len(stacks[0])              # Check level
+        if checkOdd(level):                 # If odd
+            return 0, 2                     # Move 0 to 2
+        elif not checkOdd(level):           # If even
+            return 0, 1                     # Move 0 to 1
+    # Stack 1 Moves
+    if last_move != 0 and one != 0:                             # last move wasn't to this stack and it has at least one piece
+        if one < two and checkOdd(one) != checkOdd(two):        # stack1 less than stack2 and not both odd or even
+            return 0, 1
+        elif one < three and checkOdd(one) != checkOdd(three):  # stack1 less than stack3 and not both odd or even
+            return 0, 2
+        elif two == 0:                                          # stack 2 is empty
+            return 0, 1
+        elif three == 0:                                        # stack 3 is empty
+            return 0, 2
+    # Stack 2 Moves
+    if last_move != 1 and two != 0:
+        if two < one and checkOdd(two) != checkOdd(one):
+            return 1, 0
+        elif two < three and checkOdd(two) != checkOdd(three):
+            return 1, 2
+        elif one == 0:
+            return 1, 0
+        elif three == 0:
+            return 1, 2
+    # Stack 3 Moves
+    if last_move != 2 and 3 != 0:
+        if three < two and checkOdd(three) != checkOdd(two):
+            return 2, 1
+        elif three < one and checkOdd(three) != checkOdd(one):
+            return 2, 0
+        elif one == 0:
+            return 2, 0
+        elif two == 0:
+            return 2, 1
--- test.py
+++ test.py
@@ -1,24 +1,27 @@
 import pygame as pg
+from CPU_Player import CPUMove
+import time
 
-level = 2                                           # Number of discs
-
+# Game Vars (global)
+level = 8                           # Number of discs
 window_width = 500
 window_height = 500
-
-stick_l_x = window_width*.25                        # Left stick 25% in
-stick_m_x = window_width*.5                         # Middle stick 50% in
-stick_r_x = window_width*.75                        # Right stick 75% in
-stick_x_list = [stick_l_x, stick_m_x, stick_r_x]
-
-disc_width_max = (window_width*.25) - 5
+stick_x_list = [window_width*.25, window_width*.5, window_width*.75]    # List of stick locations (25%, 50%, 75%)
+disc_width_max = (window_width*.25) - 5                                 # Max disc width just less than distance between sticks
 disc_height = 15
-
-stacks = [dict(), dict(), dict()]                   # {disc_number: rect}
-disc_move = None                                    # Default none, used to track whether user has selected a piece to move
+stacks = [dict(), dict(), dict()]   # [stack0, stack1, stack2] - {disc_number: rect}
+# User Vars (global)
+disc_move = None                    # Default none, used to track whether user has selected a piece to move [source stack index, top piece's number]
 move_counter = 0
 win_flag = False
+human_player = False
+last_move = 0                       # Track CPU's last move
+CPU_source = None                   # CPU move's source stick (index)
+CPU_target = None                   # CPU move's target stick (index)
+time_played = 0
+time_reset = 0
 
-# RGB Colors
+# RGB Colors (global)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -26,24 +29,22 @@ GREEN = (0, 255, 0)
 GOLD = (190, 145, 25)
 SILVER = (90, 90, 90)
 GREY = (220, 220, 220)
+BACKGROUND = (50, 50, 50)
 
 # Create the window display
 pg.init()
 window = pg.display.set_mode((window_width, window_height))
 pg.display.set_caption('Hanoi by B-First')
 clock = pg.time.Clock()
-GAME_FONT = pg.font.SysFont('Comic Sans MS', 30)
+text_font = pg.font.SysFont('Comic Sans MS', 30)
 
-# Create Rect to draw Ellipse
-def createEllipseRect(disc_num, stick_x, stick_i):
-    discs_in_stack = stacks[stick_i].keys()
-    num_discs_in_stack = len(discs_in_stack)
-    disc_width = (
-        disc_width_max - ((disc_width_max / (level+1)) * (level - disc_num))
-    )   # Make each disc slightly smaller than the last
-    
-    ellipse_rect = pg.Rect(                                     # Rects that ellipses are inscribed into
-        stick_x - disc_width/2,                                 # px from left, center on left stick
+# Create Rect to draw Ellipse (args: disc number, stick index)
+def createEllipseRect(disc_num, stick_i):
+    stick_x = stick_x_list[stick_i]                             # Stick x location
+    num_discs_in_stack = len(stacks[stick_i].keys())
+    disc_width = (disc_width_max - ((disc_width_max / (level+1)) * (level - disc_num)))     # Make each disc slightly smaller than the last
+    ellipse_rect = pg.Rect(                                     # Create rect that ellipse is inscribed into
+        stick_x - disc_width/2,                                 # px from left, center on stick
         window_height - (disc_height*(num_discs_in_stack+1)),   # px from top, stack discs on each other
         disc_width,
         disc_height
@@ -55,7 +56,7 @@ def drawEllipse(disc_num, stack):
     if disc_num % 2 == 0: disc_color = SILVER                   # Even discs are silver
     else: disc_color = GOLD                                     # Odd discs are gold
     pg.draw.ellipse(window, disc_color, stack[disc_num], 0)
-    pg.draw.ellipse(window, BLACK, stack[disc_num], 2)          # Add a border
+    pg.draw.ellipse(window, BLACK, stack[disc_num], 2)          # Add a disc border
 
 # Return top piece in the stack, 0 if no pieces
 def getTopPiece(stack):
@@ -65,7 +66,7 @@ def getTopPiece(stack):
 # Select a piece - return stack index and disc number, None if no pieces in stack
 def selectPiece(stack_i):
     stack = stacks[stack_i]
-    disc_num = getTopPiece(stack)
+    disc_num = getTopPiece(stacks[stack_i])
     if not disc_num: return None                        # If no piece in the stack
     pg.draw.ellipse(window, BLUE, stack[disc_num], 0)   # Redraw with blue color
     disc_move = [stack_i, disc_num]                     # Stack index and top piece's number
@@ -75,66 +76,64 @@ def selectPiece(stack_i):
 def movePiece(disc_move, stick_target_i):
     stack_source = stacks[disc_move[0]]
     disc_num = disc_move[1]
-    stick_source_x = stick_x_list[disc_move[0]]
+    stick_source_x = stick_x_list[disc_move[0]]                         # Source stick's x location
     stack_target = stacks[stick_target_i]
-    stick_target_x = stick_x_list[stick_target_i]
     disc_target = getTopPiece(stack_target)
 
-    if disc_num < disc_target or disc_target == 0:              # If the piece being moved is less than the top of the target stack
-        pg.draw.ellipse(window, GREY, stack_source[disc_num])   # Clear existing to background color
-        stack_source.pop(disc_num)                              # Remove piece from stack
-
-        stack_target[disc_num] = createEllipseRect(disc_num, stick_target_x, stick_target_i)
-        drawEllipse(disc_num, stack_target)
-    
+    if disc_num < disc_target or disc_target == 0:                              # If the piece being moved is less than the top of the target stack or no pieces on target stack
+        pg.draw.ellipse(window, BACKGROUND, stack_source[disc_num])             # Clear existing to background color
+        stack_source.pop(disc_num)                                              # Remove piece from stack
+        stack_target[disc_num] = createEllipseRect(disc_num, stick_target_i)    # Create new ellipse rect
+        drawEllipse(disc_num, stack_target)                                     # Draw new ellipse
         pg.draw.line(
             window,
             BLACK,
             (stick_source_x, window_height*.2),
             (stick_source_x, window_height)
-        )   # Redraw line so there's no gap from the piece that moved
-
+        )                                                                       # Redraw line so there's no gap from the piece that moved
         # Redraw remaining discs in source stack to cover the redrawn line
-        for disc_num in stack_source:                           # Loop through dict keys (disc_nums)
+        for disc_num in stack_source:                                           # Loop through dict keys (disc_nums)
             drawEllipse(disc_num, stack_source)
-        
-        return 1                                                # Return 1 to increment move counter
-    else:
-        drawEllipse(disc_num, stack_source)                     # Turn it from selected (blue) to not
-        return 0                                                # Return 0 to not increment move counter
+        return 1                                                                # Return 1 to increment move counter
+    else:                                                                       # Invalide move
+        drawEllipse(disc_num, stack_source)                                     # Turn it from selected (blue) to not
+        return 0                                                                # Return 0 to not increment move counter
 
 # Set up the game
 def resetGame():
+    # Clear game vars
     stacks[0].clear()
     stacks[1].clear()
     stacks[2].clear()
     global win_flag; win_flag = False
     global move_counter; move_counter = 0
-    window.fill(GREY)
-    
+    global time_reset; time_reset = pg.time.get_ticks()
+    global time_played; time_played = 0
+    window.fill(BACKGROUND)
     # Draw the 3 sticks
-    pg.draw.line(window, BLACK, (stick_l_x, window_height*.2), (stick_l_x, window_height))
-    pg.draw.line(window, BLACK, (stick_m_x, window_height*.2), (stick_m_x, window_height))
-    pg.draw.line(window, BLACK, (stick_r_x, window_height*.2), (stick_r_x, window_height))
-
-    for i in reversed(range(1, level+1)):                   # Loop through from level down to 1
-        stacks[0][i] = createEllipseRect(i, stick_l_x, 0)   # Create each ellipse rect in the first stack
+    pg.draw.line(window, BLACK, (stick_x_list[0], window_height*.2), (stick_x_list[0], window_height))
+    pg.draw.line(window, BLACK, (stick_x_list[1], window_height*.2), (stick_x_list[1], window_height))
+    pg.draw.line(window, BLACK, (stick_x_list[2], window_height*.2), (stick_x_list[2], window_height))
+    # Create and draw pieces in
+    for i in reversed(range(1, level+1)):       # Loop through from level down to 1
+        stacks[0][i] = createEllipseRect(i, 0)  # Create each ellipse rect in the first stack
         drawEllipse(i, stacks[0])
-
 
 # Game Loop
 resetGame()                         # Set up the discs on the first stack
 win_stack = list(stacks[0].keys())  # List to define winning state
 while True:
-    
+
     # Update move counter drawn on surface
     pg.draw.rect(window, WHITE, (0, 0, window_width, 80))                   # Draw rect to cover previous count - left, top, width, height
-    text_move_counter = GAME_FONT.render(str(move_counter), True, BLACK)    # Create a text surface
-    window.blit(text_move_counter, (window_width/2-10, 40))                 # Draw the text surface
+    move_counter_str = str(move_counter)
+    move_counter_len = len(move_counter_str)
+    text_move_counter = text_font.render(move_counter_str, True, BLACK)    # Create a text surface
+    window.blit(text_move_counter, (window_width/2-(10*move_counter_len), 40))                 # Draw the text surface
 
     # Check if player won and print win message
     if list(stacks[2].keys()) == win_stack:                                 # If the 3rd stack has the same keys as the original stack
-        text_win = GAME_FONT.render('Congrats!!!', False, BLACK)
+        text_win = text_font.render('Congrats!!!', False, BLACK)
         window.blit(text_win, (window_width/2-70, 0))
         win_flag = True
     
@@ -145,25 +144,45 @@ while True:
             pg.quit()
             quit()
         elif event.type == pg.KEYDOWN and event.key == pg.K_RETURN:     # If Enter is pressed, reset the game
+            last_move = 0
+            CPU_source = None
+            CPU_target = None
             resetGame()
         
         if win_flag: continue                                           # If already won, don't check for moves
 
-        if event.type == pg.KEYDOWN:                                    # If a button is pressed
-            # Stack index
-            if event.key == pg.K_LEFT: stack_i = 0
-            elif event.key == pg.K_DOWN: stack_i = 1
-            elif event.key == pg.K_RIGHT: stack_i = 2
-            else: continue
-            
-            if not disc_move:                                   # If no piece selected, select it
-                disc_move = selectPiece(stack_i)
-            else:                                               # If piece selected, move it
-                move_counter += movePiece(disc_move, stack_i)   # Increment move counter (0 if failed move)
-                disc_move = None                                # Clear variable after move
-                print('Move:', move_counter)
+        if human_player:
+            if event.type == pg.KEYDOWN:                                    # If a button is pressed
+                # Stack index
+                if event.key == pg.K_LEFT: stack_i = 0
+                elif event.key == pg.K_DOWN: stack_i = 1
+                elif event.key == pg.K_RIGHT: stack_i = 2
+                else: continue
+                
+                if not disc_move:                                   # If no piece selected, select it
+                    disc_move = selectPiece(stack_i)
+                else:                                               # If piece selected, move it
+                    move_counter += movePiece(disc_move, stack_i)   # Increment move counter (0 if failed move)
+                    disc_move = None                                # Clear variable after move
+
+    if not human_player and not win_flag:
+        if CPU_source is None and CPU_target is None:               # If CPU needs to choose a move
+            CPU_source, CPU_target = CPUMove(stacks, last_move)
+        elif CPU_source is None:                                    # If piece is selected but not moved yet
+            move_counter += movePiece(disc_move, CPU_target)
+            last_move = CPU_target
+            CPU_target = None
+            disc_move = None
+        else:                                                       # If piece isn't selected yet
+            disc_move = selectPiece(CPU_source)
+            CPU_source = None
     
-    clock.tick(20)                  # Game FPS
-    #print(pg.time.get_ticks())
+    clock.tick(60)                  # Game FPS
+
+    time_current = round((pg.time.get_ticks() - time_reset)/1000, 0)
+    if time_current > time_played and not win_flag:
+        time_played = time_current
+        print(time_played)
     pg.display.update()
+    #time.sleep(.01)
 
